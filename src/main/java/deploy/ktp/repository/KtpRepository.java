4ef1618c6c7ed2d557b08b7006578ac0fba32e38
Fannandya Sutan <sfannandya@gmail.com>
2026-03-13 08:45:36 +0700

    membuat repo ktp

---
+++ src/main/java/deploy/ktp/repository/KtpRepository.java
@@ -0,0 +1,14 @@
+package deploy.ktp.repository;
+
+import deploy.ktp.model.entity.Ktp;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface KtpRepository extends JpaRepository<Ktp, Integer> {
+    Optional<Ktp> findByNomorKtp(String nomorKtp);
+}
+
+public class KtpRepository {
+}
